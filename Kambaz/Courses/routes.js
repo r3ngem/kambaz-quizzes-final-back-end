cc57a52a66a9287b230fb43eaaaee11b1356edbf
r3ngem <paclob.m@northeastern.edu>
2025-12-04 22:07:57 -0500

    bring in assignment files

---
+++ Kambaz/Courses/routes.js
@@ -0,0 +1,51 @@
+import CoursesDao from "./dao.js";
+import EnrollmentsDao from "../Enrollments/dao.js";
+export default function CourseRoutes(app, db) {
+  const dao = CoursesDao(db);
+  const enrollmentsDao = EnrollmentsDao(db);
+  const createCourse = async (req, res) => {
+    const currentUser = req.session["currentUser"];
+    const newCourse = await dao.createCourse(req.body);
+    enrollmentsDao.enrollUserInCourse(currentUser._id, newCourse._id);
+    res.json(newCourse);
+  };
+
+  const findAllCourses = async (req, res) => {
+    const courses = await dao.findAllCourses();
+    res.send(courses);
+  }
+
+  const findCoursesForEnrolledUser = async (req, res) => {
+    let { userId } = req.params;
+    if (userId === "current") {
+      const currentUser = req.session["currentUser"];
+      if (!currentUser) {
+        res.sendStatus(401);
+        return;
+      }
+      userId = currentUser._id;
+    }
+    const courses = await enrollmentsDao.findCoursesForUser(userId);
+    res.json(courses);
+  };
+
+  const deleteCourse = async (req, res) => {
+    const { courseId } = req.params;
+    await enrollmentsDao.unenrollAllUsersFromCourse(courseId);
+    const status = await dao.deleteCourse(courseId);
+    res.send(status);
+  };
+
+  const updateCourse = async (req, res) => {
+    const { courseId } = req.params;
+    const courseUpdates = req.body;
+    const status = await dao.updateCourse(courseId, courseUpdates);
+    res.send(status);
+  }
+
+  app.put("/api/courses/:courseId", updateCourse);
+  app.delete("/api/courses/:courseId", deleteCourse);
+  app.post("/api/users/current/courses", createCourse);
+  app.get("/api/courses", findAllCourses);
+  app.get("/api/users/:userId/courses", findCoursesForEnrolledUser);
+}
