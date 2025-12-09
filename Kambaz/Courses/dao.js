4ad6543899773a124d01282a683fef56fb0ce834
r3ngem <paclob.m@northeastern.edu>
2025-12-09 09:03:49 -0500

    available courses

--- Kambaz/Courses/dao.js
+++ Kambaz/Courses/dao.js
@@ -2,14 +2,14 @@ import { v4 as uuidv4 } from "uuid";
 import model from "./model.js";
 export default function CoursesDao(db) {
   function findAllCourses() {
-    return model.find({}, { name: 1, description: 1 });
+    return model.find({}, { _id: 1, name: 1, description: 1 });
   }
 
   async function findCoursesForEnrolledUser(userId) {
   const { enrollments } = db;
-  const courses = await model.find({}, { name: 1, description: 1 });
+  const courses = await model.find({}, { _id: 1, name: 1, description: 1 });
   const enrolledCourses = courses.filter((course) =>
-    enrollments.some((enrollment) => enrollment.user === userId && enrollment.course === course._id));
+    enrollments.some((enrollment) => enrollment.user === userId && enrollment.course === String(course._id)));
   return enrolledCourses;
 }
 
